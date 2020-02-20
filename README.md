a53def9a1ccc2dd364bcce40ee76501ca4cd4b57
ChrisSplinter <christoph.birtsis@its-stuttgart.de>
2020-02-20 13:53:39 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WeatherBoardProject
+Weather
---
+++ WeatherBoard/Models/OpenWeatherApiModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherBoard.Models
+{
+   public class OpenWeatherApiModel
+    {
+       
+            public double temp { get; set; }
+            public double feels_like { get; set; }
+            public double temp_min { get; set; }
+            public double temp_max { get; set; }
+            public int pressure { get; set; }
+            public int humidity { get; set; }
+        
+    }
+}

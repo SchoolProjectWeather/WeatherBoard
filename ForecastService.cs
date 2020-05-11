91374bf5d8989471420e2bd4e91aa4957e18aea4
JeromeMichelangelo <60923148+JeromeMichelangelo@users.noreply.github.com>
2020-05-11 23:08:28 +0200

    added forecast api call

---
+++ ForecastService.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TheWeatherBoard.ErrorHandling;
+using TheWeatherBoard.Models;
+
+namespace TheWeatherBoard.Services
+{
+    public class ForecastService
+    {
+        private const string APP_ID = "df1b80f131e96328741e25b186b378ba";
+        private HttpClient client;
+        public ForecastModel model;
+
+        public ForecastService()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("http://api.openweathermap.org/data/2.5/");
+
+        }
+
+        public ForecastModel GetForecast(string location)
+        {
+            var url = $"forecast?q={location}&units=metric&appid={APP_ID}";
+            var request = WebRequest.Create(client.BaseAddress + url);
+            System.IO.Stream responseStream;
+            try
+            {
+                responseStream = request.GetResponse().GetResponseStream();
+            }
+            catch
+            {
+                throw new CityNotFoundException("City not found!");
+            }
+
+
+            if (responseStream != null)
+            {
+                using (var streamReader = new StreamReader(responseStream))
+                {
+                    while (streamReader.Peek() > -1)
+                    {
+                        model = new ForecastModel();
+
+                        model = JsonConvert.DeserializeObject<ForecastModel>(streamReader.ReadLine());
+                        return model;
+                    }
+
+                }
+            }
+            else
+            {
+                throw new UnauthorizedAPIConnection("Invalid API key.");
+            }
+            throw new UnauthorizedAPIConnection("Invalid API key.");
+
+        }
+    }
+}
---
+++ MainViewModel.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Newtonsoft.Json;
+using Renci.SshNet.Messages;
+using TheWeatherBoard.Bases;
+using TheWeatherBoard.Helper;
+using TheWeatherBoard.Models;
+using TheWeatherBoard.Services;
+
+namespace TheWeatherBoard.ViewModels
+{
+  public class MainViewModel: ViewModelBase
+    {
+
+        public CalcuteConverter calcuteConverter;
+        public CurrentWeatherService currentWeatherService;
+        public ForecastService forecastService;
+
+        public IconPick iconPick;
+        public ButtonCommandBase ShowWeatherCommand { get; private set; }
+        public ButtonCommandBase ForcastCommand { get; }
+
+        public MainViewModel()
+        {  
+            iconPick = new IconPick();
+            calcuteConverter = new CalcuteConverter();
+            currentWeatherService = new CurrentWeatherService();
+            ShowWeatherCommand = new ButtonCommandBase(ShowWeather);
+            ForcastCommand = new ButtonCommandBase(ShowForecast);
+            forecastService = new ForecastService();
+        }
+
+        private void ShowWeather()
+        {
+            CurrentWeatherModel model = new CurrentWeatherModel();
+            model = currentWeatherService.GetCurrentWeather(Location);
+
+            Temperature = model.main.temp + " C°";
+            Description = model.weather[0].description;
+            TempFeelsLike = "Feels Like: " + model.main.feels_like + " C°";
+            TempMin = "Min Temperature: " + model.main.temp_min + " C°";
+            TempMax = "Max Temperature: " + model.main.temp_max + " C°";
+            WindSpeed = "Wind Speed: " + model.wind.speed + "m/s";
+            City = model.name;
+            Sunrise = calcuteConverter.UnixTimeStampConverter(model.sys.sunrise);
+            Sunset = calcuteConverter.UnixTimeStampConverter(model.sys.sunset);
+            WeatherIcon = iconPick.pickIcon(model.weather[0].icon);
+        }
+
+        private void ShowForecast()
+        {
+            ForecastModel model = new ForecastModel();
+            model = forecastService.GetForecast(Location);
+
+            string test;
+            foreach (var x in model.list)
+            {
+               test = x.main.temp.ToString() + x.main.temp.ToString();
+                MessageBox.Show(test);
+            }
+
+
+            // Use this to Debug Forecast Output
+            JsonConvert.SerializeObject(model, Formatting.Indented);
+
+
+        }
+
+
+        private string _listTemp;
+        public string ListTemp
+        {
+            get { return _listTemp; }
+            set
+            {
+                _listTemp = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _weatherIcon;
+        public string WeatherIcon
+        {
+            get { return _weatherIcon; }
+            set
+            {
+                _weatherIcon = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _location;
+        public string Location
+        {
+            get { return _location; }
+            set
+            {
+                _location = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string _temperature;
+        public string Temperature
+        {
+            get { return _temperature; }
+            set
+            {
+                _temperature = value;
+                OnPropertyChanged();
+            }
+        }
+
+    
+
+        private string _descrition;
+        public string Description
+        {
+            get { return _descrition; }
+            set
+            {
+                _descrition = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _tempFeelsLike;
+        public string TempFeelsLike
+        {
+            get { return _tempFeelsLike; }
+            set
+            {
+                _tempFeelsLike = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _tempMax;
+        public string TempMax
+        {
+            get { return _tempMax; }
+            set
+            {
+                _tempMax = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _tempMin;
+        public string TempMin
+        {
+            get { return _tempMin; }
+            set
+            {
+                _tempMin = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _windSpeed;
+        public string WindSpeed
+        {
+            get { return _windSpeed; }
+            set
+            {
+                _windSpeed = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _city;
+        public string City
+        {
+            get { return _city; }
+            set
+            {
+                _city = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _sunset;
+        public string Sunset
+        {
+            get { return _sunset; }
+            set
+            {
+                _sunset = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _sunrise;
+        public string Sunrise
+        {
+            get { return _sunrise; }
+            set
+            {
+                _sunrise = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
